9721124f8824fca6ec6fd086bf876c156f418d8d
Xinyang <xinyzhang9@gmail.com>
2017-11-10 16:00:04 +0800

    fetch with basic schema

---
+++ schema.js
@@ -0,0 +1,49 @@
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLNonNull
+} = require('graphql');
+
+const customers = [
+  {id: '1', name: 'Xinyang', email:'xinyang@gmail.com', age: 20},
+  {id: '2', name: 'Mike', email:'mike@gmail.com', age: 25},
+  {id: '3', name: 'John', email:'john@gmail.com', age: 32},
+];
+
+const CustomerType = new GraphQLObjectType({
+  name: 'Customer',
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
+    email: { type: GraphQLString},
+  },
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    customer: {
+      type: CustomerType,
+      args: {
+        id: { type: GraphQLString },
+      },
+      resolve(parentValue, args) {
+        return customers.find((x) => x.id === args.id);
+      },
+    },
+    customers: {
+      type: new GraphQLList(CustomerType),
+      resolve(parentValue, args) {
+        return customers;
+      }
+    }
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express')
-
+const expressGraphQL = require('express-graphql');
 const app = express();
+
+const schema = require('./schema');
+
+app.use('/graphql',expressGraphQL({
+  schema: schema,
+  graphiql: true,
+}))
 
 app.listen(3000, ()=> {
   console.log(`server running on port 3000 ...`);
